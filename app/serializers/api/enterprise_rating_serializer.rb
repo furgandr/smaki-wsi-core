5e373274e8cd4268fbf69ef0b914042015b7afe1
endriu <andrzej@afurgal.com>
2026-01-16 19:03:23 +0100

    zmien kto pisze komentarze na Imie...

--- app/serializers/api/enterprise_rating_serializer.rb
+++ app/serializers/api/enterprise_rating_serializer.rb
@@ -5,9 +5,18 @@ module Api
     attributes :rating, :comment, :created_at, :author_name, :excluded_from_stats, :excluded_reason
 
     def author_name
-      user = object.user || object.order&.user
-      label = user&.login.presence || user&.email.to_s.split("@").first
-      mask_label(label)
+      order = object.order
+      user = object.user || order&.user
+      first = order&.bill_address&.firstname || user&.first_name || user&.firstname
+      last = order&.bill_address&.lastname || user&.last_name || user&.lastname
+
+      if first.present?
+        masked_last = mask_last_name(last)
+        [first, masked_last].reject(&:blank?).join(" ").strip
+      else
+        label = user&.login.presence || user&.email.to_s.split("@").first
+        mask_label(label)
+      end
     end
 
     def mask_label(label)
@@ -17,6 +26,14 @@ module Api
       return "#{text.first}***" if text.length < 4
 
       "#{text.first(2)}***#{text.last(2)}"
+    end
+
+    def mask_last_name(last_name)
+      return "" if last_name.to_s.strip.empty?
+
+      text = last_name.to_s
+      visible = text[-2, 2] || text
+      ("*" * [text.length - 2, 0].max) + visible
     end
   end
 end
