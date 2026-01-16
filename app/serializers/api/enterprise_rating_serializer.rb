e9d83fe2f332aa331c87b8d8ea478d462520a321
endriu <andrzej@afurgal.com>
2026-01-16 14:11:50 +0100

    dodany panel opinie do menu admina

--- app/serializers/api/enterprise_rating_serializer.rb
+++ app/serializers/api/enterprise_rating_serializer.rb
@@ -5,10 +5,18 @@ module Api
     attributes :rating, :comment, :created_at, :author_name, :excluded_from_stats, :excluded_reason
 
     def author_name
-      address = object.order&.bill_address
-      return "" unless address
+      user = object.user || object.order&.user
+      label = user&.login.presence || user&.email.to_s.split("@").first
+      mask_label(label)
+    end
 
-      "#{address.firstname} #{address.lastname}".strip
+    def mask_label(label)
+      return "" if label.to_s.strip.empty?
+
+      text = label.to_s
+      return "#{text.first}***" if text.length < 4
+
+      "#{text.first(2)}***#{text.last(2)}"
     end
   end
 end
