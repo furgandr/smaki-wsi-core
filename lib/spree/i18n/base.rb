9ba1e9230a44c9f3107e5d8352155e95fff66fca
endriu <andrzej@afurgal.com>
2026-01-31 15:38:30 +0100

    fix error 500

--- lib/spree/i18n/base.rb
+++ lib/spree/i18n/base.rb
@@ -21,6 +21,18 @@ end
 # Zeitwerk expects this constant to match the file path.
 module Spree
   module I18n
+    class << self
+      def method_missing(name, *args, &block)
+        return ::I18n.public_send(name, *args, &block) if ::I18n.respond_to?(name)
+
+        super
+      end
+
+      def respond_to_missing?(name, include_private = false)
+        ::I18n.respond_to?(name, include_private) || super
+      end
+    end
+
     module Base
     end
   end
