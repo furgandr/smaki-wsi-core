018f67acbad5ec71f068842f60dde67c15206769
endriu <andrzej@afurgal.com>
2026-01-31 14:49:10 +0100

    fix sidekiq

--- lib/spree/i18n/initializer.rb
+++ lib/spree/i18n/initializer.rb
@@ -1,3 +1,11 @@
 # frozen_string_literal: true
 
+# Zeitwerk expects this constant to match the file path.
+module Spree
+  module I18n
+    module Initializer
+    end
+  end
+end
+
 ApplicationController.include(Spree::ViewContext)
