47cbf78223812dcc3ac57d4ced0277640736a557
endriu <andrzej@afurgal.com>
2026-01-31 14:55:51 +0100

    Zeitwerk oczekuje stałej Tasks::Data::RemoveTransientData

--- lib/tasks/data/remove_transient_data.rb
+++ lib/tasks/data/remove_transient_data.rb
@@ -1,49 +1,54 @@
 # frozen_string_literal: true
 
-class RemoveTransientData
-  RETENTION_PERIOD = 3.months
+# Zeitwerk expects this constant to match the file path.
+module Tasks
+  module Data
+    class RemoveTransientData
+      RETENTION_PERIOD = 3.months
 
-  # This model lets us operate on the sessions DB table using ActiveRecord's
-  # methods within the scope of this service. This relies on the AR's
-  # convention where a Session model maps to a sessions table.
-  class Session < ApplicationRecord
-  end
+      # This model lets us operate on the sessions DB table using ActiveRecord's
+      # methods within the scope of this service. This relies on the AR's
+      # convention where a Session model maps to a sessions table.
+      class Session < ApplicationRecord
+      end
 
-  attr_reader :expiration_date
+      attr_reader :expiration_date
 
-  def initialize
-    @expiration_date = RETENTION_PERIOD.ago.to_date
-  end
+      def initialize
+        @expiration_date = RETENTION_PERIOD.ago.to_date
+      end
 
-  def call
-    Rails.logger.info("#{self.class.name}: processing")
+      def call
+        Rails.logger.info("#{self.class.name}: processing")
 
-    Spree::StateChange.where(created_at: ...expiration_date).delete_all
-    Spree::LogEntry.where(created_at: ...expiration_date).delete_all
-    Session.where(updated_at: ...expiration_date).delete_all
+        Spree::StateChange.where(created_at: ...expiration_date).delete_all
+        Spree::LogEntry.where(created_at: ...expiration_date).delete_all
+        Session.where(updated_at: ...expiration_date).delete_all
 
-    clear_old_cart_data!
-  end
+        clear_old_cart_data!
+      end
 
-  private
+      private
 
-  def clear_old_cart_data!
-    old_carts = Spree::Order.
-      where("spree_orders.state = 'cart' AND spree_orders.updated_at < ?", expiration_date).
-      merge(orders_without_payments)
+      def clear_old_cart_data!
+        old_carts = Spree::Order.
+          where("spree_orders.state = 'cart' AND spree_orders.updated_at < ?", expiration_date).
+          merge(orders_without_payments)
 
-    old_cart_line_items = Spree::LineItem.where(order_id: old_carts)
-    old_cart_adjustments = Spree::Adjustment.where(order_id: old_carts)
+        old_cart_line_items = Spree::LineItem.where(order_id: old_carts)
+        old_cart_adjustments = Spree::Adjustment.where(order_id: old_carts)
 
-    old_cart_adjustments.delete_all
-    old_cart_line_items.delete_all
-    old_carts.delete_all
-  end
+        old_cart_adjustments.delete_all
+        old_cart_line_items.delete_all
+        old_carts.delete_all
+      end
 
-  def orders_without_payments
-    # Carts with failed payments are ignored, as they contain potentially useful data
-    Spree::Order.
-      joins("LEFT OUTER JOIN spree_payments ON spree_orders.id = spree_payments.order_id").
-      where(spree_payments: { id: nil })
+      def orders_without_payments
+        # Carts with failed payments are ignored, as they contain potentially useful data
+        Spree::Order.
+          joins("LEFT OUTER JOIN spree_payments ON spree_orders.id = spree_payments.order_id").
+          where(spree_payments: { id: nil })
+      end
+    end
   end
 end
